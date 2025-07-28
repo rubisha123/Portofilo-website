0e71d494e63cc408b0a3b88e48d085d5a0716da3
Rubi Sha <165404615+rubisha123@users.noreply.github.com>
2025-07-28 09:07:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+form.addEventListener("submit", function (e) {
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (!name || !email || !message) {
+    e.preventDefault();
+    alert("Please fill out all fields.");
+  } else {
+    e.preventDefault(); // still prevent real submit since no backend
+    alert(" Your message has been 'sent' (demo mode)");
+    form.reset();
+  }
+});
+
